5d154fd88c265490542a8f6a9414a9a2eedfa6a9
Friedrich Lindenberg <friedrich@pudo.org>
2019-02-02 20:22:29 +0100

    extract code from aleph

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # aleph-recognize-text
-A Tesseract 4 gRPC service container for optical character generation
+
+This is a service container that performs OCR on images
+submitted as a byte stream by using `tesseract` 4.0. The
+input should specify the language, if possible, so the
+right type of script detection can be used.
+
+Images submitted via the API should be limited to less
+than 4 MiB in size, if possible, to avoid problems with
+the gRPC protocol.
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='textrecognizer',
+    version='0.0.1',
+    author='Organized Crime and Corruption Reporting Project',
+    author_email='pudo@occrp.org',
+    url='https://occrp.org',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=[],
+    include_package_data=True,
+    package_data={},
+    install_requires=[
+        'pyicu',
+        'pillow',
+        'grpcio',
+        'pytest',
+        'normality',
+        'tesserocr',
+        'languagecodes>=1.0.4',
+        'alephclient==0.7.1'
+    ],
+    zip_safe=False
+)
